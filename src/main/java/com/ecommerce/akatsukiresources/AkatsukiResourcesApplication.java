730a9a7ed1f1d8b868738b4e2d9e8e70c0c203c8
William Ohia <ohiawilliam@gmail.com>
2022-11-22 13:02:09 -0500

    build

--- src/main/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplication.java
+++ src/main/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
 @SpringBootApplication
 public class AkatsukiResourcesApplication {
 
--- src/main/java/com/ecommerce/akatsukiresources/service/AppUserService.java
+++ src/main/java/com/ecommerce/akatsukiresources/service/AppUserService.java
@@ -11,10 +11,7 @@ import com.ecommerce.akatsukiresources.repository.AppUserRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.DatatypeConverter;
