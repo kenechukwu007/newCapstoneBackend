1f376f7e8c41dba3fd1748faa1c93d3b83728192
William Ohia <ohiawilliam@gmail.com>
2022-11-22 12:00:06 -0500

    fixing unit test

--- src/test/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplicationTests.java
+++ src/test/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplicationTests.java
@@ -2,8 +2,11 @@ package com.ecommerce.akatsukiresources;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("sandbox")
 class AkatsukiResourcesApplicationTests {
 
 	@Test
