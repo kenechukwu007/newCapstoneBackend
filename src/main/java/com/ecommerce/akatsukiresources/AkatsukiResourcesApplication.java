66538e5f920b03143e05de7ad9d5c29ac668de37
William Ohia <ohiawilliam@gmail.com>
2022-11-27 21:22:27 -0500

    added some annotation

--- src/main/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplication.java
+++ src/main/java/com/ecommerce/akatsukiresources/AkatsukiResourcesApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class AkatsukiResourcesApplication {
 
 	public static void main(String[] args) {
