21d8c4445b98727cc84cc07d58064d662333ed0e
William Ohia <ohiawilliam@gmail.com>
2022-11-27 21:16:58 -0500

    added annotation to one of the repositories

--- src/main/java/com/ecommerce/akatsukiresources/repository/ShoppingCartRepo.java
+++ src/main/java/com/ecommerce/akatsukiresources/repository/ShoppingCartRepo.java
@@ -3,9 +3,11 @@ package com.ecommerce.akatsukiresources.repository;
 import com.ecommerce.akatsukiresources.model.Appuser;
 import com.ecommerce.akatsukiresources.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ShoppingCartRepo extends JpaRepository<ShoppingCart, Integer> {
 
     List<ShoppingCart> findAllByUser(Appuser user);
