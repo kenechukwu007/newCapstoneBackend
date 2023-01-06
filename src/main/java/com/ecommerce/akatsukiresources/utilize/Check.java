637d075a4837cc8844975bc520a0b6de160f1a5c
William Ohia <ohiawilliam@gmail.com>
2023-01-05 22:39:16 -0500

    changed db

--- src/main/java/com/ecommerce/akatsukiresources/utilize/Check.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.akatsukiresources.utilize;
-
-import javax.validation.constraints.NotNull;
-
-public class Check {
-    public static boolean isPresent(Object object){
-        return object != null;
-    }
-}
